ba0eb5167bb4bb3dc953c6c532463cc7726f55fe
Chris Borckholder <borckholder@gmail.com>
2013-05-21 13:26:44 +0200

    Integration -> Functional tests
     renamed marker interface
     WebClient handling encapsulated in JUnit Rule

--- asio-server/src/test/java/at/ac/univie/isc/asio/acceptance/SqlQueryTest.java
+++ asio-server/src/test/java/at/ac/univie/isc/asio/acceptance/SqlQueryTest.java
@@ -2,7 +2,6 @@ package at.ac.univie.isc.asio.acceptance;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_XML_TYPE;
 import static javax.ws.rs.core.Response.Status.Family.CLIENT_ERROR;
-import static javax.ws.rs.core.Response.Status.Family.SERVER_ERROR;
 import static javax.ws.rs.core.Response.Status.Family.SUCCESSFUL;
 import static javax.ws.rs.core.Response.Status.Family.familyOf;
 import static org.junit.Assert.assertEquals;
@@ -10,31 +9,35 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URI;
 
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.client.WebClient;
 import org.apache.cxf.jaxrs.ext.form.Form;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import at.ac.univie.isc.asio.IntegrationTest;
-import at.ac.univie.isc.asio.TestUtils;
+import at.ac.univie.isc.asio.FunctionalTest;
+import at.ac.univie.isc.asio.JaxrsClientProvider;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
 
-@Category(IntegrationTest.class)
+@Category(FunctionalTest.class)
 public class SqlQueryTest {
 
 	/* slf4j-logger */
 	final static Logger log = LoggerFactory.getLogger(SqlQueryTest.class);
+
+	private static final URI SERVER_URL = URI
+			.create("http://localhost:8080/v1/asio/query");
 
 	private static final String PARAM_QUERY = "query";
 	private static final String COUNT_QUERY = "SELECT COUNT(*) FROM person";
@@ -51,27 +54,24 @@ public class SqlQueryTest {
 	private WebClient client;
 	private Response response;
 
+	@Rule public JaxrsClientProvider provider = new JaxrsClientProvider(
+			SERVER_URL);
+
 	@Before
 	public void setUp() {
-		client = WebClient.create("http://localhost:8080/v1/asio/query");
-		client.accept(APPLICATION_XML_TYPE);
-	}
-
-	@After
-	public void tearDown() {
-		dumpIfError(response);
-		client.reset();
+		client = provider.getClient();
 	}
 
 	@Test
 	public void valid_query_as_uri_param() throws Exception {
-		client.query(PARAM_QUERY, COUNT_QUERY);
+		client.accept(APPLICATION_XML_TYPE).query(PARAM_QUERY, COUNT_QUERY);
 		response = client.get();
 		verify(response);
 	}
 
 	@Test
 	public void valid_query_as_form_param() throws Exception {
+		client.accept(APPLICATION_XML_TYPE);
 		final Form values = new Form();
 		values.set(PARAM_QUERY, COUNT_QUERY);
 		response = client.form(values);
@@ -80,7 +80,7 @@ public class SqlQueryTest {
 
 	@Test
 	public void valid_query_as_payload() throws Exception {
-		client.type("application/sql-query");
+		client.accept(APPLICATION_XML_TYPE).type("application/sql-query");
 		response = client.post(COUNT_QUERY);
 		verify(response);
 	}
@@ -88,7 +88,7 @@ public class SqlQueryTest {
 	@Ignore
 	@Test
 	public void delivers_csv() throws Exception {
-		client.reset().accept(CSV).query(PARAM_QUERY, SCAN_QUERY);
+		client.accept(CSV).query(PARAM_QUERY, SCAN_QUERY);
 		response = client.get();
 		assertEquals(SUCCESSFUL, familyOf(response.getStatus()));
 		assertTrue(CSV.isCompatible(response.getMediaType()));
@@ -106,8 +106,7 @@ public class SqlQueryTest {
 	@Ignore
 	@Test
 	public void delivers_xml() throws Exception {
-		client.reset().accept(MediaType.APPLICATION_XML)
-				.query(PARAM_QUERY, SCAN_QUERY);
+		client.accept(MediaType.APPLICATION_XML).query(PARAM_QUERY, SCAN_QUERY);
 		response = client.get();
 		assertEquals(SUCCESSFUL, familyOf(response.getStatus()));
 		assertTrue(XML.isCompatible(response.getMediaType()));
@@ -124,7 +123,7 @@ public class SqlQueryTest {
 
 	@Test
 	public void bad_query_parameter() throws Exception {
-		client.query(PARAM_QUERY, "");
+		client.accept(APPLICATION_XML_TYPE).query(PARAM_QUERY, "");
 		response = client.get();
 		assertEquals(CLIENT_ERROR, familyOf(response.getStatus()));
 	}
@@ -132,11 +131,5 @@ public class SqlQueryTest {
 	private void verify(final Response response) {
 		assertEquals(SUCCESSFUL, familyOf(response.getStatus()));
 		assertTrue(APPLICATION_XML_TYPE.isCompatible(response.getMediaType()));
-	}
-
-	private void dumpIfError(final Response response) {
-		if (SERVER_ERROR == familyOf(response.getStatus())) {
-			log.error(TestUtils.toString(response));
-		}
 	}
 }
