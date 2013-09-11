41c0ce10478056c494ddaed11987cdcf022319cd
Chris Borckholder <borckholder@gmail.com>
2013-09-11 17:26:26 +0200

    FIX - VariantConverter now sets required encoding to null instead of charset

--- asio-core/src/main/java/at/ac/univie/isc/asio/frontend/FormatSelector.java
+++ asio-core/src/main/java/at/ac/univie/isc/asio/frontend/FormatSelector.java
@@ -14,6 +14,9 @@ import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Variant;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import at.ac.univie.isc.asio.DatasetOperation;
 import at.ac.univie.isc.asio.DatasetOperation.Action;
 import at.ac.univie.isc.asio.DatasetOperation.SerializationFormat;
@@ -25,106 +28,95 @@ import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.Iterables;
 
 /**
- * Maintain a mapping of {@link DatasetOperation.Action actions} to
- * {@link Variant variant}->{@link SerializationFormat format} pairs derived
- * from the set of supported formats given on creation.
+ * Maintain a mapping of {@link DatasetOperation.Action actions} to {@link Variant variant}->
+ * {@link SerializationFormat format} pairs derived from the set of supported formats given on
+ * creation.
  * 
  * @author Chris Borckholder
  */
 public class FormatSelector {
 
-	private static final String UNSUPPORTED_MESSAGE = "%s not supported";
+  /* slf4j-logger */
+  private final static Logger log = LoggerFactory.getLogger(FormatSelector.class);
 
-	private final VariantConverter converter;
-	private final Map<Action, Map<Variant, SerializationFormat>> mappingsByAction;
+  private static final String UNSUPPORTED_MESSAGE = "%s not supported";
 
-	public FormatSelector(final Set<SerializationFormat> supported,
-			final VariantConverter converter) {
-		super();
-		this.converter = converter;
-		mappingsByAction = initializeMappings(supported);
-	}
+  private final VariantConverter converter;
+  private final Map<Action, Map<Variant, SerializationFormat>> mappingsByAction;
 
-	/**
-	 * Attempt to match an accepted {@link Variant} from the given request to a
-	 * {@link SerializationFormat format} supported for the given action.
-	 * 
-	 * @param request
-	 *            holding acceptable variants
-	 * @param action
-	 *            of operation
-	 * @return selected variant
-	 * @throws WebApplicationException
-	 *             with status 406 (Not Acceptable) if no variant matches or
-	 *             status 405 (Method Not Allowed) if the given action is not
-	 *             supported
-	 */
-	public SerializationFormat selectFormat(final Request request,
-			final Action action) {
-		final Map<Variant, SerializationFormat> mapping = mappingsByAction
-				.get(action);
-		if (mapping.isEmpty()) {
-			// TODO throw custom DatasetUsageException
-			throw new WebApplicationException(Response
-					.status(METHOD_NOT_ALLOWED)
-					.allow(Collections.<String> emptySet())
-					.entity(format(ENGLISH, UNSUPPORTED_MESSAGE, action))
-					.build());
-		}
-		final List<Variant> candidates = ImmutableList.copyOf(mapping.keySet());
-		final Variant selected = request.selectVariant(candidates);
-		if (selected == null) {
-			// TODO throw custom DatsetUsageException
-			throw new WebApplicationException(Response
-					.notAcceptable(candidates).build());
-		}
-		return mapping.get(selected);
-	}
+  public FormatSelector(final Set<SerializationFormat> supported, final VariantConverter converter) {
+    super();
+    this.converter = converter;
+    mappingsByAction = initializeMappings(supported);
+  }
 
-	/**
-	 * @param supported
-	 *            given formats
-	 * @return {@link Variant}->{@link SerializationFormat} mappings for each
-	 *         {@link Action}.
-	 */
-	private Map<Action, Map<Variant, SerializationFormat>> initializeMappings(
-			final Set<SerializationFormat> supported) {
-		final Builder<Action, Map<Variant, SerializationFormat>> mappings = ImmutableMap
-				.builder();
-		for (final Action action : Action.values()) {
-			mappings.put(action, mappingsFrom(filteredBy(action, supported)));
-		}
-		return mappings.build();
-	}
+  /**
+   * Attempt to match an accepted {@link Variant} from the given request to a
+   * {@link SerializationFormat format} supported for the given action.
+   * 
+   * @param request holding acceptable variants
+   * @param action of operation
+   * @return selected variant
+   * @throws WebApplicationException with status 406 (Not Acceptable) if no variant matches or
+   *         status 405 (Method Not Allowed) if the given action is not supported
+   */
+  public SerializationFormat selectFormat(final Request request, final Action action) {
+    final Map<Variant, SerializationFormat> mapping = mappingsByAction.get(action);
+    if (mapping.isEmpty()) {
+      // TODO throw custom DatasetUsageException
+      throw new WebApplicationException(Response.status(METHOD_NOT_ALLOWED)
+          .allow(Collections.<String>emptySet())
+          .entity(format(ENGLISH, UNSUPPORTED_MESSAGE, action)).build());
+    }
+    final List<Variant> candidates = ImmutableList.copyOf(mapping.keySet());
+    final Variant selected = request.selectVariant(candidates);
+    if (selected == null) {
+      log.debug("!! no acceptable variant in {}", candidates); // XXX remove when handled in
+                                                               // exception
+      // TODO throw custom DatsetUsageException
+      throw new WebApplicationException(Response.notAcceptable(candidates).build());
+    }
+    return mapping.get(selected);
+  }
 
-	/**
-	 * @param iterable
-	 * @return Variant->format map
-	 */
-	private Map<Variant, SerializationFormat> mappingsFrom(
-			final Iterable<SerializationFormat> iterable) {
-		final Builder<Variant, SerializationFormat> variant2Format = ImmutableMap
-				.builder();
-		for (final SerializationFormat each : iterable) {
-			variant2Format.put(converter.asVariant(each.asMediaType()), each);
-		}
-		return variant2Format.build();
-	}
+  /**
+   * @param supported given formats
+   * @return {@link Variant}->{@link SerializationFormat} mappings for each {@link Action}.
+   */
+  private Map<Action, Map<Variant, SerializationFormat>> initializeMappings(
+      final Set<SerializationFormat> supported) {
+    final Builder<Action, Map<Variant, SerializationFormat>> mappings = ImmutableMap.builder();
+    for (final Action action : Action.values()) {
+      mappings.put(action, mappingsFrom(filteredBy(action, supported)));
+    }
+    return mappings.build();
+  }
 
-	/**
-	 * @param by
-	 *            action that must be supported by filtered formats
-	 * @param formats
-	 *            to be filtered
-	 * @return iterable with filtered formats
-	 */
-	private Iterable<SerializationFormat> filteredBy(
-			final Action by, final Set<SerializationFormat> formats) {
-		return Iterables.filter(formats, new Predicate<SerializationFormat>() {
-			@Override
-			public boolean apply(final SerializationFormat input) {
-				return input.applicableOn(by);
-			}
-		});
-	}
+  /**
+   * @param iterable
+   * @return Variant->format map
+   */
+  private Map<Variant, SerializationFormat> mappingsFrom(
+      final Iterable<SerializationFormat> iterable) {
+    final Builder<Variant, SerializationFormat> variant2Format = ImmutableMap.builder();
+    for (final SerializationFormat each : iterable) {
+      variant2Format.put(converter.asVariant(each.asMediaType()), each);
+    }
+    return variant2Format.build();
+  }
+
+  /**
+   * @param by action that must be supported by filtered formats
+   * @param formats to be filtered
+   * @return iterable with filtered formats
+   */
+  private Iterable<SerializationFormat> filteredBy(final Action by,
+      final Set<SerializationFormat> formats) {
+    return Iterables.filter(formats, new Predicate<SerializationFormat>() {
+      @Override
+      public boolean apply(final SerializationFormat input) {
+        return input.applicableOn(by);
+      }
+    });
+  }
 }
--- asio-core/src/main/java/at/ac/univie/isc/asio/frontend/VariantConverter.java
+++ asio-core/src/main/java/at/ac/univie/isc/asio/frontend/VariantConverter.java
@@ -9,49 +9,45 @@ import javax.ws.rs.core.Variant;
 import com.google.common.base.Optional;
 
 /**
- * Convert guava {@link com.google.common.net.MediaType media types} to
- * corresponding {@link Variant JAXRS variants}.
+ * Convert guava {@link com.google.common.net.MediaType media types} to corresponding
+ * {@link Variant JAXRS variants}.
  * 
  * @author Chris Borckholder
  */
 public class VariantConverter {
 
-	public VariantConverter() {
-		super();
-	}
+  public VariantConverter() {
+    super();
+  }
 
-	/**
-	 * Create a new variant matching the given media type as close as possible.
-	 * 
-	 * @param given
-	 *            guava media type
-	 * @return matching JAXRS variant
-	 */
-	public Variant asVariant(final com.google.common.net.MediaType given) {
-		final MediaType type = asContentType(given);
-		final Locale language = Locale.ENGLISH;
-		final String encoding = encodingOrNullFor(given.charset());
-		return new Variant(type, language, encoding);
-	}
+  /**
+   * Create a new variant matching the given media type as close as possible.
+   * 
+   * @param given guava media type
+   * @return matching JAXRS variant
+   */
+  public Variant asVariant(final com.google.common.net.MediaType given) {
+    final MediaType type = asContentType(given);
+    final Locale language = Locale.ENGLISH;
+    final String encoding = null; // omitted
+    return new Variant(type, language, encoding);
+  }
 
-	/**
-	 * Create a new JAXRS Mediatype matching the given guava type as close as
-	 * possible.
-	 * 
-	 * @param given
-	 *            guava media type
-	 * @return matching JAXRS media type
-	 */
-	public MediaType asContentType(final com.google.common.net.MediaType given) {
-		return new MediaType(given.type(), given.subtype(),
-				encodingOrNullFor(given.charset()));
-	}
+  /**
+   * Create a new JAXRS Mediatype matching the given guava type as close as possible.
+   * 
+   * @param given guava media type
+   * @return matching JAXRS media type
+   */
+  public MediaType asContentType(final com.google.common.net.MediaType given) {
+    return new MediaType(given.type(), given.subtype(), encodingOrNullFor(given.charset()));
+  }
 
-	private String encodingOrNullFor(final Optional<Charset> charset) {
-		if (charset.isPresent()) {
-			return charset.get().toString();
-		} else {
-			return null;
-		}
-	}
+  private String encodingOrNullFor(final Optional<Charset> charset) {
+    if (charset.isPresent()) {
+      return charset.get().toString();
+    } else {
+      return null;
+    }
+  }
 }
