0a51fc44757f7428181781537554fe27fd152d9c
Yuriy Kaniovskyi <Yuriy.Kaniovskyi@univie.ac.at>
2014-03-05 18:19:08 +0100

    Data browser impl

--- asio-integration/src/main/webapp/static/js/browser.app.js
+++ asio-integration/src/main/webapp/static/js/browser.app.js
@@ -39,9 +39,28 @@ var Controller = new function () {
         schema : null,
         result : null,
       };
+	  
+	  /*********************/
+  this.fetchRowCount = function (table) {
+    var query = "SELECT COUNT(*) FROM `" + table +"`";
+	asio.executeQuery(query, function (error, xml) {
+      if (xml) {
+        var result = asio.parseWebrowset(xml);
+		return result.rows[0].row;
+	  } else {
+        alert(error);
+      }
+    });
+  };  
   
-  this.fetchTable = function (table) {
-    this.executeQuery("SELECT * FROM `" + table +"` LIMIT 20");
+  this.fetchTable = function (table, limit) {
+    this.executeQuery("SELECT * FROM `" + table +"` LIMIT "+limit);
+  };
+  
+  this.fetchTableField = function (table, field, limit) {
+	//-1 due to space
+	field = field.substring(0, field.lastIndexOf("(")-1); 
+	this.executeQuery("SELECT DISTINCT COUNT(`" + field + "`) AS occurence, `" + field +"` FROM `" + table +"` GROUP BY `"+ field +"` ORDER BY occurence DESC LIMIT "+limit);
   };
   
   this.executeQuery = function (query) {
@@ -80,9 +99,24 @@ var Controller = new function () {
 
 //==============================================================>
 // ACTION HOOKS
-function onSelectTable(event, tableName) {
+function onSelectTable(event, tableName, limit) {
   event.preventDefault();
-  Controller.fetchTable(tableName);
+  event.stopPropagation();
+  Controller.fetchTable(tableName, limit);
+};
+
+function onSelectLimit(){ // set limit according to select box
+	var query = $('#sql-command').val();
+	//edit query to set limit
+	//+6 due to the key word limit and the following space
+	query = query.substring(0,query.toUpperCase().lastIndexOf("LIMIT")+6) + $("#limitselect").val();
+    Controller.executeQuery(query);
+};
+
+function onSelectField(event, fieldName, limit) {
+  event.preventDefault();
+  event.stopPropagation();
+  Controller.fetchTableField($(event.target).parent().parent().attr("xasiotablename"), fieldName, limit);
 };
 
 function overrideForm() {
@@ -95,11 +129,49 @@ function overrideForm() {
   });
 }; 
 
+function downloadTable() {
+	$('#sql-download-form').submit(function(event){ //listen for submit event
+		alert($('#sql-command').val());
+	
+		$('#sql-download-command').val($('#sql-command').val())
+		console.log($('#sql-download-command').val());
+		return true;
+		
+    });
+}; 
+
+//makes sql-explorer list collapsable
+function prepareDBList() {
+	$('#dblist').find('li:has(ul)').unbind('click').click(function(event) {
+		if(this == event.target) {
+			$(this).toggleClass('expanded');
+			$(this).children('ul').toggle('medium');
+		}
+		return false;
+	}).addClass('collapsed').removeClass('expanded').children('ul').hide();
+ 
+	//Hack to add links inside the cv
+	$('#dblist a').unbind('click').click(function() {
+		window.open($(this).attr('href'));
+		return false;
+	});
+	//Create the button functionality
+	/*$('#expandList').unbind('click').click(function() {
+		$('.collapsed').addClass('expanded');
+		$('.collapsed').children().show('medium');
+	})
+	$('#collapseList').unbind('click').click(function() {
+		$('.collapsed').removeClass('expanded');
+		$('.collapsed').children().hide('medium');
+	})*/
+}
+
 //==============================================================>
 // BOOTSTRAPPING
 function main() {
   Controller.sync();
   overrideForm();
+  prepareDBList();
 };
 
 $(document).ready(main);
