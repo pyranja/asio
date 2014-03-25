1bd359eae4857a28b8c0967781f72230ea7b34b6
Chris Borckholder <borckholder@gmail.com>
2014-03-25 11:07:39 +0100

    maven restructuring : multi-module -> simple project  ~ integration and core modules merged  ~ top level pom and module pom merged  ~ module folders removed - project collapsed into root folder

---
+++ src/main/java/uk/org/ogsadai/context/OgsadaiContextLoaderListener.java
@@ -0,0 +1,95 @@
+package uk.org.ogsadai.context;
+
+import java.io.File;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+/**
+ * Initialize the {@link uk.org.ogsadai.context.OGSADAIContext} on webapp start using the bean definition file specified as
+ * context parameter {@link #CONFIG_FILE_PARAMETER ogsadaiConfigLocation}.
+ * 
+ * @author Chris Borckholder
+ */
+public class OgsadaiContextLoaderListener implements ServletContextListener {
+
+  // use jdk logging to avoid initialization races
+  // redirects to JULI on tomcat
+  private static final Logger log = Logger.getLogger(OgsadaiContextLoaderListener.class.getName());
+
+  // web app context parameter
+  public static final String CONFIG_FILE_PARAMETER = "ogsadaiConfigLocation";
+  // path extensions
+  private static final String SCHEMA_PATH = "schema";
+  private static final String CONFIG_PATH = "WEB-INF/etc/dai";
+
+  @Override
+  public void contextInitialized(final ServletContextEvent event) {
+    log.config("initializing ogsadai context");
+    final File root = resolveRoot(event.getServletContext());
+    log.config("using " + root + " as root path");
+    final ApplicationContext parent;
+    try {
+      parent = initializeParent(root);
+    } catch (final ClassNotFoundException e) {
+      throw new IllegalStateException("ogsadai initialization - failed to generate parent context",
+          e);
+    }
+    final String ogsadaiConfigLocation = readConfigLocation(event.getServletContext());
+    log.config("using " + ogsadaiConfigLocation + " as ogsadai context");
+    try {
+      OGSADAIContext.initialize(ogsadaiConfigLocation, parent);
+    } catch (final Exception e) {
+      throw new IllegalStateException("ogsadai initialization - failed to create OGSADAIContext", e);
+    }
+    OGSADAIContext.getInstance().logEntries();
+  }
+
+  /**
+   * Determine the web-application's root directory
+   */
+  private File resolveRoot(final ServletContext context) {
+    final String rootPath = context.getRealPath("/");
+    return new File(rootPath);
+  }
+
+  /**
+   * reads and validates the ogsadai config location parameter from the given ServletContext.
+   */
+  private String readConfigLocation(final ServletContext context) {
+    final String location = context.getInitParameter(CONFIG_FILE_PARAMETER);
+    if (location == null || location.trim().isEmpty()) {
+      throw new IllegalStateException("ogsadai initialization - init param ['"
+          + CONFIG_FILE_PARAMETER + "'] empty or missing in web.xml");
+    }
+    return location;
+  }
+
+  /**
+   * Generate a parent {@link org.springframework.context.ApplicationContext} holding the dynamic references to important
+   * application directories.
+   * 
+   * @throws ClassNotFoundException if registering directory beans fails
+   */
+  private ApplicationContext initializeParent(final File root) throws ClassNotFoundException {
+    final GenericApplicationContext parent = new GenericApplicationContext();
+    SpringUtils.registerFileBean(parent, OGSADAIConstants.WEB_APP_DIR.toString(), root);
+    final File schemaDirectory = new File(root, SCHEMA_PATH);
+    SpringUtils.registerFileBean(parent, OGSADAIConstants.SCHEMA_DIR.toString(), schemaDirectory);
+    final File configDirectory = new File(root, CONFIG_PATH);
+    SpringUtils.registerFileBean(parent, OGSADAIConstants.CONFIG_DIR.toString(), configDirectory);
+    parent.refresh();
+    return parent;
+  }
+
+  @Override
+  public void contextDestroyed(final ServletContextEvent event) {
+    log.config("destroying ogsadai context");
+    // XXX clean up needed here ?
+  }
+}
---
+++ src/test/java/at/ac/univie/isc/asio/sql/ResultSetIterator.java
@@ -0,0 +1,113 @@
+package at.ac.univie.isc.asio.sql;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Iterator;
+
+import javax.annotation.Nonnull;
+
+import com.google.common.collect.AbstractIterator;
+
+/**
+ * Turns a ResultSet into an Iterator using a given {@link at.ac.univie.isc.asio.sql.ResultSetIterator.RowConverter} to process each row of the
+ * ResultSet into an object.
+ * 
+ * @author Chris Borckholder
+ * @param <T> type of converted rows
+ */
+public final class ResultSetIterator<T> extends AbstractIterator<T> implements AutoCloseable {
+
+  /**
+   * Create an Iterator over the given {@link java.sql.ResultSet}.
+   * 
+   * @param resultSet will be iterated
+   * @param converter converts each row of resultSet to an object
+   * @return the iterator
+   */
+  public static <T> ResultSetIterator<T> from(final ResultSet resultSet,
+      final RowConverter<? extends T> converter) {
+    return new ResultSetIterator<T>(resultSet, converter);
+  }
+
+  /**
+   * Convert the given {@link java.sql.ResultSet} to an {@link Iterable} over the conversion results of the
+   * given {@link at.ac.univie.isc.asio.sql.ResultSetIterator.RowConverter}. <strong>Caution:</strong>This iterable can only be traversed once!
+   * 
+   * @return an Iterable suitable for for-each constructs
+   */
+  public static <T> Iterable<T> asIterable(final ResultSet resultSet,
+      final RowConverter<? extends T> converter) {
+    return new Iterable<T>() {
+      @Override
+      public Iterator<T> iterator() {
+        return new ResultSetIterator<T>(resultSet, converter);
+      }
+    };
+  }
+
+  /**
+   * Defines a conversion function for individual ResultSet rows.
+   * 
+   * @author Chris Borckholder
+   * @param <ROW> type of conversion results
+   */
+  // @FunctionalInterface - java8
+  public static interface RowConverter<ROW> {
+    /**
+     * Convert the given ResultSet to an object of type ROW. Should not modify or advance the given
+     * ResultSet. Implementations should <strong>never</strong> produce null as conversion result.
+     * 
+     * @param resultSet set to current row
+     * @return result of conversion (non-null)
+     * @throws java.sql.SQLException occurring while interacting with the ResultSet
+     */
+    @Nonnull
+    ROW process(@Nonnull ResultSet resultSet) throws SQLException;
+  }
+
+  /**
+   * Thrown if any (Sql)Exception occurs while iterating the ResultSet.
+   * 
+   * @author Chris Borckholder
+   */
+  public static class ResultSetIterationException extends IllegalStateException {
+    private static final long serialVersionUID = 7447190500968931002L;
+
+    public ResultSetIterationException(final Throwable cause) {
+      super(cause);
+    }
+  }
+
+  private final ResultSet rs;
+  private final RowConverter<? extends T> converter;
+
+  private ResultSetIterator(final ResultSet resultSet, final RowConverter<? extends T> converter) {
+    super();
+    this.rs = checkNotNull(resultSet, "cannot iterate null result set");
+    this.converter = checkNotNull(converter, "cannot use null function as converter");
+  }
+
+  @Override
+  protected T computeNext() {
+    try {
+      if (rs.next()) {
+        return converter.process(rs);
+      } else {
+        return endOfData();
+      }
+    } catch (final SQLException e) {
+      throw new ResultSetIterationException(e);
+    }
+  }
+
+  /**
+   * Closes the {@link java.sql.ResultSet} that was/is iterated.
+   */
+  @Override
+  public void close() throws SQLException {
+    rs.close();
+  }
+
+}
