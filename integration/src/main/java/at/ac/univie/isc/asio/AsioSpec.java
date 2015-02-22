6dfe0d6461cbc0a5469199150fdcd2fbafa8a02e
Chris Borckholder <borckholder@gmail.com>
2015-02-22 13:53:21 +0100

    [DEV] support for header permissions in AsioSpec
    Add AsioSpec variant, that uses request headers to transmit granted
    Role.

--- integration/src/main/java/at/ac/univie/isc/asio/AsioSpec.java
+++ integration/src/main/java/at/ac/univie/isc/asio/AsioSpec.java
@@ -1,5 +1,6 @@
 package at.ac.univie.isc.asio;
 
+import com.jayway.restassured.authentication.PreemptiveBasicAuthScheme;
 import com.jayway.restassured.builder.RequestSpecBuilder;
 import com.jayway.restassured.specification.RequestSpecification;
 
@@ -11,6 +12,17 @@ import static java.util.Objects.requireNonNull;
  * Deployment specific configuration for rest-assured integration tests.
  */
 public abstract class AsioSpec {
+  /**
+   * Authorization is applied by sending the permission in the request headers.
+   *
+   * @param address base service address
+   * @param header name of the auth bearing header
+   * @return header based asio spec
+   */
+  public static AsioSpec withHeaderAuthorization(final URI address, final String header) {
+    return new HeaderBased(address, header);
+  }
+
   /**
    * Authorization is applied by including the permission in the request URI.
    *
@@ -59,6 +71,37 @@ public abstract class AsioSpec {
   public abstract URI authorizedAddress(final String permission);
 
   public abstract boolean isAuthorizing();
+
+  /**
+   * Send permission as http header and force basic auth.
+   */
+  private static class HeaderBased extends AsioSpec {
+    private final String headerName;
+    private final PreemptiveBasicAuthScheme defaultAuth;
+
+    private HeaderBased(final URI root, final String headerName) {
+      super(root);
+      this.headerName = requireNonNull(headerName, "header name");
+      defaultAuth = new PreemptiveBasicAuthScheme();
+      defaultAuth.setUserName("anonymous");
+      defaultAuth.setPassword("");
+    }
+
+    @Override
+    protected RequestSpecBuilder authorize(final RequestSpecBuilder spec, final String permission) {
+      return spec.setAuth(defaultAuth).addHeader(headerName, permission);
+    }
+
+    @Override
+    public URI authorizedAddress(final String permission) {
+      return this.getRoot();
+    }
+
+    @Override
+    public boolean isAuthorizing() {
+      return true;
+    }
+  }
 
   /**
    * Use the URI based authorization mechanism
