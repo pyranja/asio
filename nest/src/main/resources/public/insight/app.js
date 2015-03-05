c7e0cb3a375bad7b95515afd715c708b61d0b941
Chris Borckholder <borckholder@gmail.com>
2015-03-05 12:00:16 +0100

    Port events UI to nest
    Add bootstrap and angularjs as webjar dependencies. events angular app
    included in nest, served by spring mvc default static content handlers.

---
+++ nest/src/main/resources/public/insight/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var asioApp = angular.module('asioApp', []);
+
+asioApp.controller('EventMonitor', function ($scope) {
+  $scope.events = [];
+  var handler = function (event) {
+    $scope.$apply(function () {
+      $scope.events.push(event);
+    });
+  }
+  var feed = new EventSource('/catalog/events');
+  feed.addEventListener('request', handler, false);
+  feed.addEventListener('stream', handler, false);
+});
