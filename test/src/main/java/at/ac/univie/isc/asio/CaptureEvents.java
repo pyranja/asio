0c9b69023dc0f3d493298fac5992236e5116ba53
Chris Borckholder <borckholder@gmail.com>
2015-03-27 15:59:40 +0100

    Support 'reset' and document CaptureEvents
    Add javadoc to CaptureEvents methods and add #reset() to enable clearing
    of captured events, e.g. when tests have to prepare the SUT and want to
    ignore events from the preparation phase.

--- test/src/main/java/at/ac/univie/isc/asio/CaptureEvents.java
+++ test/src/main/java/at/ac/univie/isc/asio/CaptureEvents.java
@@ -9,11 +9,20 @@ import com.google.common.eventbus.Subscribe;
 import java.util.List;
 
 /**
- * Capture events from a guava {@code EventBus}.
+ * Capture events from a guava {@code EventBus}. <strong>ALL</strong> events posted to the provided
+ * {@link #bus() EventBus} are captured. The generic type signals which events are expected to be
+ * caught for convenient checking.
  *
- * @param <EVENT>
+ * @param <EVENT> type of events that are expected
  */
 public final class CaptureEvents<EVENT> {
+  /**
+   * Create a capturer, with convenience methods for given type of events.
+   *
+   * @param type   class of expected events
+   * @param <TYPE> type of events as given by {@code type} parameter
+   * @return event capturer for given type
+   */
   public static <TYPE> CaptureEvents<TYPE> create(final Class<TYPE> type) {
     return new CaptureEvents<>(type);
   }
@@ -29,22 +38,48 @@ public final class CaptureEvents<EVENT> {
     eventBus.register(new CaptureSubscriber());
   }
 
+  /**
+   * The {@code EventBus} that this capturer is subscribed to. It is a fully functional event bus.
+   *
+   * @return test event bus
+   */
   public EventBus bus() {
     return eventBus;
   }
 
+  /**
+   * Drop all events captured until now.
+   */
+  public void reset() {
+    captured.clear();
+  }
+
+  /**
+   * Get all captured events, including events that are not of the expected type.
+   */
   public Iterable<Object> all() {
     return captured;
   }
 
+  /**
+   * Get all captured events, that are instances of the expected type or its sub classes,
+   * in the same order as they were captured.
+   */
   public Iterable<EVENT> captured() {
     return Iterables.filter(captured, type);
   }
 
+  /**
+   * Get the single captured event of the expected type. Fails fast if more than one event was
+   * captured.
+   */
   public EVENT single() {
     return Iterables.getOnlyElement(captured());
   }
 
+  /**
+   * Get the i-th captured event of the expected type.
+   */
   public EVENT get(final int index) {
     assert index < captured.size();
     return Iterators.get(captured().iterator(), index);
