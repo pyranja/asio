6897278aef5c9eb8146b566ad3761f4fdf00f87f
Chris Borckholder <borckholder@gmail.com>
2015-04-04 23:48:53 +0200

    Remove @Component from EventBusAutoRegistrar and add logging

--- nest/src/main/java/at/ac/univie/isc/asio/spring/EventBusAutoRegistrar.java
+++ nest/src/main/java/at/ac/univie/isc/asio/spring/EventBusAutoRegistrar.java
@@ -1,25 +1,28 @@
 package at.ac.univie.isc.asio.spring;
 
+import at.ac.univie.isc.asio.Scope;
 import com.google.common.eventbus.EventBus;
+import org.slf4j.Logger;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.stereotype.Component;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * Pass all beans to the configured {@link com.google.common.eventbus.EventBus} for registration.
  */
-@Component
 public final class EventBusAutoRegistrar implements BeanPostProcessor {
+  private static final Logger log = getLogger(EventBusAutoRegistrar.class);
+
   private final EventBus eventBus;
 
-  @Autowired
   public EventBusAutoRegistrar(final EventBus eventBus) {
     this.eventBus = eventBus;
   }
 
   @Override
   public Object postProcessAfterInitialization(final Object bean, final String beanName) throws BeansException {
+    log.trace(Scope.SYSTEM.marker(), "event bus registration of {}({})", beanName, bean.getClass());
     eventBus.register(bean);
     return bean;
   }
