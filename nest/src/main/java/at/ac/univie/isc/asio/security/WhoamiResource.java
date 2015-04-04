07d47637b22a9da3996387ae02fc8aaa6af3439e
Chris Borckholder <borckholder@gmail.com>
2015-04-04 23:55:58 +0200

    Add standalone path to WhoamiResource

--- nest/src/main/java/at/ac/univie/isc/asio/security/WhoamiResource.java
+++ nest/src/main/java/at/ac/univie/isc/asio/security/WhoamiResource.java
@@ -11,7 +11,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Component
-@Path("/")
+@Path("/whoami")
 public class WhoamiResource {
   private final SecurityContext security;
 
