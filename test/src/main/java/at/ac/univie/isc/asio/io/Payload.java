4a7efb5d5dbcaa764f15d938b9e0ac1badd380cc
Chris Borckholder <borckholder@gmail.com>
2015-04-11 18:41:57 +0200

    Add Paylod#randomText
    Generates fixed length strings with alpha-numeric content.

--- test/src/main/java/at/ac/univie/isc/asio/io/Payload.java
+++ test/src/main/java/at/ac/univie/isc/asio/io/Payload.java
@@ -17,6 +17,7 @@ public final class Payload {
 
   /**
    * Generate a fixed size {@code byte[]} with random contents.
+   *
    * @param size of generated array
    * @return randomly filled byte array
    */
@@ -27,9 +28,27 @@ public final class Payload {
     return payload;
   }
 
+  private static final char[] ALPHA_NUM = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+
+  /**
+   * Generate a fixed size {@code String} with random, alpha-numeric content.
+   *
+   * @param length length of generated text
+   * @return random, alpha-numeric string
+   */
+  public static String randomText(final int length) {
+    assert length >= 0 : "payload cannot have negative size";
+    final char[] content = new char[length];
+    for (int i = 0; i < content.length; i++) {
+      content[i] = ALPHA_NUM[RNG.nextInt(ALPHA_NUM.length)];
+    }
+    return String.copyValueOf(content);
+  }
+
   /**
    * Convert raw utf-8 encoded data back to {@code String}. It is not checked whether the data is actually
    * utf-8 encoded.
+   *
    * @param data raw binary data of an utf-8 encoded string
    * @return decoded data as String
    */
@@ -39,6 +58,7 @@ public final class Payload {
 
   /**
    * Convert a {@code String} to utf-8 encoded data.
+   *
    * @param text to be converted
    * @return utf-8 encoded binary data
    */
@@ -48,6 +68,7 @@ public final class Payload {
 
   /**
    * Copy the content from given {@code ByteSource} to an array.
+   *
    * @param source source of binary data
    * @return byte array containing the contents of the {@code source}
    * @throw UncheckedIOException if reading the source fails
