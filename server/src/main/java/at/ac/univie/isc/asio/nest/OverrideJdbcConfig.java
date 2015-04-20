def3a134e82ef8cf275b28f59f75646562981516
Chris Borckholder <borckholder@gmail.com>
2015-04-20 18:23:01 +0200

    JdbcOverride Configurer

---
+++ server/src/main/java/at/ac/univie/isc/asio/nest/OverrideJdbcConfig.java
@@ -0,0 +1,55 @@
+package at.ac.univie.isc.asio.nest;
+
+import at.ac.univie.isc.asio.AsioSettings;
+import at.ac.univie.isc.asio.Brood;
+import at.ac.univie.isc.asio.database.Jdbc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * If global jdbc connection settings are present, override the config in any deployed container.
+ * All properties, except for the name of the backing database schema, are discarded.
+ */
+@Brood
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ConditionalOnProperty("asio.jdbc")
+final class OverrideJdbcConfig implements Configurer {
+  private final Jdbc override;
+
+  /** required for component scanning */
+  @Autowired
+  OverrideJdbcConfig(final AsioSettings config) {
+    this(config.jdbc);
+  }
+
+  OverrideJdbcConfig(final Jdbc override) {
+    requireNonNull(override, "override jdbc connection config is null!");
+    this.override = override;
+  }
+
+  @Nonnull
+  @Override
+  public NestConfig apply(final NestConfig input) {
+    input.getJdbc()
+        .setUrl(override.getUrl())
+        .setDriver(override.getDriver())
+        .setUsername(override.getUsername())
+        .setPassword(override.getPassword())
+        .setProperties(new HashMap<>(override.getProperties()));
+    return input;
+  }
+
+  @Override
+  public String toString() {
+    return "OverrideJdbcConfig{" +
+        "override=" + override +
+        '}';
+  }
+}
---
+++ server/src/test/java/at/ac/univie/isc/asio/nest/OverrideJdbcConfigTest.java
@@ -0,0 +1,57 @@
+package at.ac.univie.isc.asio.nest;
+
+import at.ac.univie.isc.asio.database.Jdbc;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+public class OverrideJdbcConfigTest {
+
+  @Test
+  public void should_keep_dataset_and_mapping() throws Exception {
+    final OverrideJdbcConfig subject = new OverrideJdbcConfig(new Jdbc());
+    final NestConfig initial = NestConfig.empty();
+    final NestConfig processed = subject.apply(initial);
+    assertThat(processed.getDataset(), sameInstance(initial.getDataset()));
+    assertThat(processed.getMapping(), sameInstance(initial.getMapping()));
+  }
+
+  @Test
+  public void should_replace_jdbc_connection_props_with_global() throws Exception {
+    final Map<String, String> props = new HashMap<>();
+    props.put("key", "value");
+    final Jdbc override = new Jdbc()
+        .setUrl("jdbc:asio:override")
+        .setDriver("MyJdbcDriver")
+        .setUsername("override-username")
+        .setPassword("override-password")
+        .setProperties(props);
+    final OverrideJdbcConfig subject = new OverrideJdbcConfig(override);
+    final Jdbc processed = subject.apply(NestConfig.empty()).getJdbc();
+    assertThat(processed.getUrl(), equalTo("jdbc:asio:override"));
+    assertThat(processed.getDriver(), equalTo("MyJdbcDriver"));
+    assertThat(processed.getUsername(), equalTo("override-username"));
+    assertThat(processed.getPassword(), equalTo("override-password"));
+    assertThat(processed.getProperties(), equalTo(props));
+  }
+
+  @Test
+  public void should_not_use_the_mutable_global_instance_as_override() throws Exception {
+    final Jdbc override = new Jdbc();
+    final Jdbc processed = new OverrideJdbcConfig(override).apply(NestConfig.empty()).getJdbc();
+    assertThat(processed, not(sameInstance(override)));
+  }
+
+  @Test
+  public void should_not_override_schema() throws Exception {
+    final Jdbc override = new Jdbc().setSchema("override");
+    final NestConfig initial = NestConfig.empty();
+    initial.getJdbc().setSchema("original");
+    final NestConfig processed = new OverrideJdbcConfig(override).apply(initial);
+    assertThat(processed.getJdbc().getSchema(), equalTo("original"));
+  }
+}
