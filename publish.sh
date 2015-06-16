421f61b739a9c6fb1cb7b4fe18d1646278759339
Chris Borckholder <borckholder@gmail.com>
2015-06-16 15:19:41 +0200

    Publish rpm to bintray after build
    Add 'publish.sh' shell script, that can publish rpm artifacts to bintray.
    Required inputs are the artifact version and the bintray api key.

---
+++ publish.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# =============================================================================
+# publish rpm artifact on bintray
+# =============================================================================
+set +x  # disable xtrace to hide api key
+set -ev
+
+VERSION=${1:?'missing artifact version'}
+BINTRAY_KEY=${2:?'missing bintray api key'}
+
+API='https://api.bintray.com/'
+
+RPM_PATH="./distribution/target/publish"
+RPM_FILE="${RPM_PATH}/asio-distribution-${VERSION}.rpm"
+
+# download the rpm artifact from local or remote repo
+rm -f "${RPM_PATH}/*.rpm"
+mvn dependency:copy -Dartifact=at.ac.univie.isc:asio-distribution:${VERSION}:rpm -DoutputDirectory=${RPM_PATH}/ -Dmdep.overwriteReleases=true
+
+# upload to bintray
+CURL="curl -u pyranja:${BINTRAY_KEY} -H Content-Type:application/json -H Accept:application/json --write-out %{http_code} --output /dev/stderr --silent --show-error"
+
+[[ $(${CURL} -T "${RPM_FILE}" "${API}/content/pyranja/rpm/asio/${VERSION}/asio-distribution-${VERSION}.rpm?publish=1&override=0") -eq 201 ]] \
+&& echo "at.ac.univie.isc:asio:${VERSION} rpm deployed successfully"
